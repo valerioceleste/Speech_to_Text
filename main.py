922daf8e3f12ebb634c4aa65d2f467d9e4bf306e
Valerio Celeste <valerioceleste@192.168.1.9>
2023-08-27 00:35:16 +0200

    mp3 to wav working

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import mp3_to_wav
+from mp3_to_wav import wav_converter
 
 # Provide paths for input MP3 and output WAV files
-mp3_file_path = "../Speech_to_Text/Audio/long_audio.mp3"
-wav_file_path = "../Speech_to_Text/Audio/output.wav"
+mp3_file_path = "../Audio/long_audio.mp3"
+wav_file_path = "../Audio/output.wav"
 
-mp3_to_wav.wav_converter(mp3_file_path, wav_file_path)
+wav_converter(mp3_file_path, wav_file_path, channels=1)
--- mp3_to_wav.py
+++ mp3_to_wav.py
@@ -4,9 +4,10 @@ from pydub import AudioSegment
 def wav_converter(mp3_path, wav_path, channels=1):
 
     try:
-        sound = AudioSegment.from_file(mp3_file_path, format="mp3")
+        sound = AudioSegment.from_file(mp3_path, format="mp3")
     except:
-        sound = AudioSegment.from_file(mp3_file_path, format="mp4")
+        sound = AudioSegment.from_file(mp3_path, format="mp4")
     finally:
         sound = sound.set_channels(channels)
         sound.export(wav_path, format="wav")
+        print('...converting mp3 to wav.')
